6b1c96093d603535b047064ccf818459c7f67e8f
Berkson Ximenes <berksonx@yahoo.com.br>
2026-01-19 22:02:52 -0300

    Criar config do banco de dados

---
+++ src/test/java/com/berkson/wish/wishlist/model/ProductTest.java
@@ -0,0 +1,12 @@
+package com.berkson.wish.wishlist.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created By : Berkson Ximenes
+ * Date : 19/01/2026
+ **/
+
+class ProductTest {
+
+}
