141f6c486afb641bda1b27f15aa1208e72b41526
berkson <berksonx@yahoo.com.br>
2026-01-22 13:06:54 -0300

    Deixar configuração do cucumber para o Spring

--- src/test/java/com/berkson/wish/wishlist/bdd/CucumberSpringConfig.java
+++ src/test/java/com/berkson/wish/wishlist/bdd/CucumberSpringConfig.java
@@ -2,28 +2,15 @@ package com.berkson.wish.wishlist.bdd;
 
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 /**
  * Created By : Berkson Ximenes
  * Date : 20/01/2026
- **/
+ * 
+ * Configuração para usar o MongoDB que já está rodando via Docker Compose.
+ * Certifique-se de que o MongoDB esteja rodando: docker compose up -d
+ */
 @CucumberContextConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Testcontainers
 public class CucumberSpringConfig {
-
-    @Container
-    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0")
-            .withExposedPorts(27017);
-
-    @DynamicPropertySource
-    static void setProps(DynamicPropertyRegistry registry) {
-        mongoDBContainer.start();
-        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
-    }
 }
